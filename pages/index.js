e5d6901a24178f660e3f7e58225dba8e8d56e53d
Abdul Rehman <beingabdulr@gmail.com>
2023-03-17 22:57:01 +0500

    Added Loading screen

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,26 @@
 import Head from "next/head";
+import { useState, useEffect } from "react";
 import styles from "@/styles/Home.module.css";
 import Layout from "@/components/Layout";
 import Hero from "@/sections/Hero";
 import AboutUs from "@/sections/AboutUs";
 import Mission from "@/sections/Mission";
 import Join from "@/sections/Join";
+import { Suspense } from "react";
+import Loading from "@/components/Loading";
 
 export default function Home() {
-	return (
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setIsLoading(false);
+		}, 2000);
+	}, []);
+
+	return isLoading ? (
+		<Loading />
+	) : (
 		<>
 			<Head>
 				<title>CGR Consulting</title>
@@ -26,10 +39,12 @@ export default function Home() {
 			</Head>
 			<Layout>
 				<main className=''>
-					<Hero />
-					<AboutUs />
-					<Mission />
-					<Join />
+					<Suspense fallback={<p>Loading...</p>}>
+						<Hero />
+						<AboutUs />
+						<Mission />
+						<Join />
+					</Suspense>
 				</main>
 			</Layout>
 		</>
